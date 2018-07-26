1eba29d29d4fce158283ca267971fb8b98875553
Ronaldo Ortiz <ronyortizop@yahoo.com>
2018-07-26 09:58:09 -0400

    Initial commit - VID 010 done

---
+++ basics/numbers.js
@@ -0,0 +1,20 @@
+/* let x = 11;
+let num = (x + 1) * 2;
+console.log(num); */
+
+let age = 45;
+let dogYears = (age + 1) / 7;
+console.log(dogYears);
+
+// Challenge area 
+// studentScore 18
+// maxScore 20
+// percent
+// print the percent
+
+let studentScore = 18;
+let maxScore = 20;
+let percent = studentScore / maxScore;
+let printResult = "The student percentage is " + percent * 100 + "%.";
+console.log(printResult);
+
---
+++ basics/strings.js
@@ -0,0 +1,6 @@
+//let name = "Ronaldo Ortiz";
+let firstName = "Ronaldo";
+let lastName = "Ortiz";
+let fullName = firstName + ' ' + lastName;
+
+console.log(fullName);
